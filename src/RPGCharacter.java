a99a45e6a660cb0144466130c0f4d8a34c785b1e
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-08-31 13:04:36 -0400

    RpgCharacter Completed

--- src/RPGCharacter.java
+++ src/RPGCharacter.java
@@ -1,3 +1,5 @@
+import javax.swing.JOptionPane;
+
 public class RPGCharacter {
 
     /*Create a program that calculates an RPG Character's battle stats based on the character's physical stats.
@@ -10,4 +12,34 @@ public class RPGCharacter {
 
      The program should run using dialog boxes and the output should show ALL the character's stats including a name.
      */
+
+    public static void main(String[] args) {
+
+      //Setting base Stats
+        int strength = Integer.parseInt(JOptionPane.showInputDialog(" What is your Character's strength stat? (Roll a d20) "));
+        int dexterity = Integer.parseInt(JOptionPane.showInputDialog (" Whats your Charater's dexterity stat? (Roll a d20) "));
+        int constitution = Integer.parseInt(JOptionPane.showInputDialog(" Whats your character's constitution? (Roll a d20) "));
+        int wisdom = Integer.parseInt(JOptionPane.showInputDialog(" What is your Character's Wisdom stat? (Roll a d20) "));
+        int charisma = Integer.parseInt(JOptionPane.showInputDialog(" What is your character's charisma stat? (Roll a d20) "));
+        int intelligence = Integer.parseInt(JOptionPane.showInputDialog("what is your character's Intelligence stat? (Roll a d20) "));
+
+     //Calculating  advance stats
+        double damage = 3 * (strength + wisdom) * dexterity;
+        double armor = (constitution + dexterity) * 2;
+        double mana = ((wisdom + charisma) * constitution) + intelligence;
+        double spellPower = ((wisdom + charisma) * strength * intelligence + dexterity);
+
+     //returning information
+JOptionPane.showMessageDialog(null, " Strength - " + strength + " Dexterity - " + dexterity + " Constitution - " + constitution + " Wisdom - " + wisdom + " Charisma - " + charisma + " Intelligence - " + intelligence );
+JOptionPane.showMessageDialog(null, " Base damage - " + damage + " Armor rating - " + armor + " Mana capacity - " + mana + " Spell power - " + spellPower );
+
+
+
+
+
+
+
+
+
+    }
 }
