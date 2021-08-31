666fb4b0afba4c4d1dab2045f4c84bafcce13612
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-08-31 13:26:21 -0400

    MadLib.java 1/3 completed

--- src/MadLib.java
+++ src/MadLib.java
@@ -1,6 +1,28 @@
+import java.util.Scanner;
+
 public class MadLib {
 
     /*Create your own MadLibs. (https://www.madtakes.com/) You may use a story or example from the
     website if you don't wish to make up your own. This program must run entirely in the console.
      */
+
+    public static void main(String[] args) {
+
+        Scanner read = new Scanner(System.in);
+
+        System.out.println(" What is a silly word? ");
+        String sillyWordOne = read.nextLine();
+        System.out.println(" What is a random last name? ");
+        String lastName = read.nextLine();
+        System.out.println(" what's a random Illness?");
+        String illness = read.nextLine();
+
+
+        System.out.println(" Dear School Nurse: " + sillyWordOne + lastName + " will not be attending school today. He/She has come down with a case of ");
+
+
+
+
+
+    }
 }
