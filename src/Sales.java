31e48ce099500314cbd4122a43683f71416a3a4d
HaruYuki-Ar1ta <Campbcha000@warren.k12.in.us>
2021-09-01 13:21:50 -0400

    1/2 slaes done

--- src/Sales.java
+++ src/Sales.java
@@ -1,3 +1,7 @@
+import javax.swing.JOptionPane;
+import java.text.DecimalFormat;
+
+
 public class Sales {
 
     /*Write a program, using dialog boxes, which prompts the user to enter a quantity, price, discount(as an integer) and sales tax(as an integer).
@@ -16,4 +20,33 @@ public class Sales {
     Tax Total: $1.57
     Total Cost: $24.03
      */
+
+    public static void main(String[] args) {
+
+     int quantity = Integer.parseInt(JOptionPane.showInputDialog("How many items are you buying? "));
+     double pricePer = Double.parseDouble(JOptionPane.showInputDialog("How much is the item you are purchasing? "));
+     int discount = Integer.parseInt(JOptionPane.showInputDialog("Do you have a discount? "));
+     int salesTax = Integer.parseInt(JOptionPane.showInputDialog("What is sales tax? "));
+
+     DecimalFormat currency = new DecimalFormat("###.##");
+
+     double price =  pricePer * quantity;
+     double subtotal = price - (price * (discount / 100));
+     double taxTotal = price - (price * (salesTax / 100));
+     double totalCost = subtotal + taxTotal ;
+
+     JOptionPane.showMessageDialog(" Your price is: " + totalCost + " Your subtotal is: " + subtotal + " Your Tax is: " + taxtTotal);
+
+
+
+
+
+
+
+
+
+
+
+    }
+
 }
